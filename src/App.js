fb9b5873f856471776d5e86f3b1eb2bbe71b4edf
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-06 18:29:10 -0400

    Selectors adding. Some functions written.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import './App.css';
 
 function App() {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const remainingDays =  startDate.getMonth() + 1
+
+  console.log({startDate})
+  console.log({remainingDays})
+
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://cdn-icons-png.flaticon.com/512/5351/5351550.png" className="App-logo" alt="logo" />
         <h1>Rent Proration Calculator</h1>
-        <p>
-          This will be a calculator used to determine rent proration.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+        </div>
+
+        <input type="text" placeholder="Cost"></input>
+
+        <select>
+          <option value=''></option>
+          <option>Move-in</option>
+          <option>Move-out</option>
+        </select>
+  
+      
+        <button>Calculate</button>
+
+        <h2>Amount owed</h2>
+
+        <div></div>
       </header>
+      <body>
+
+      </body>
     </div>
   );
 }
