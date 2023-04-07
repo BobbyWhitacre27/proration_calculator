ea796b750aad99bd0e69e111f4b0ed582a13a1e5
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-06 22:24:42 -0400

    Calculations work 100%

--- src/App.js
+++ src/App.js
@@ -5,57 +5,84 @@ import './App.css';
 
 function App() {
   const [startDate, setStartDate] = useState(new Date());
+  const [cost, setCost] = useState(0)
+  const [moveStatus, setMoveStatus] = useState("")
+  const [result, setResult] = useState(0)
 
   const getDaysInMonth = (year, month) => {
     return new Date(year, month, 0).getDate();
-  } 
+  }
 
   const currentDate = startDate.getDate()
-  const currentMonth =  startDate.getMonth() + 1
+  const currentMonth = startDate.getMonth() + 1
   const currentYear = startDate.getFullYear()
 
   const daysInCurrentMonth = getDaysInMonth(currentYear, currentMonth)
 
-  const ratio = currentDate / daysInCurrentMonth
-  
+  const moveInRatio = (daysInCurrentMonth + 1 - currentDate) / daysInCurrentMonth
 
-  console.log({startDate})
-  console.log({currentDate})
-  console.log({currentMonth})
-  console.log({currentYear})
-  console.log({daysInCurrentMonth})
-  console.log({ratio})
+  const moveOutRatio = currentDate / daysInCurrentMonth
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="https://cdn-icons-png.flaticon.com/512/5351/5351550.png" className="App-logo" alt="logo" />
-        <h1>Rent Proration Calculator</h1>
+  const handleCost = event => {
+    setCost(event.target.value)
+  }
 
-        <div>
-          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-        </div>
+  const handleMoveStatus = event => {
+    setMoveStatus(event.target.value)
+  }
 
-        <input type="text" placeholder="Cost"></input>
+  const handleCalculate = () => {
+    if (moveStatus === "moveOut") {
+      const moveOutCost = moveOutRatio * cost
+      return setResult(moveOutCost)
+    }
+    else if (moveStatus === "moveIn") {
+      const moveInCost = moveInRatio * cost
+      return setResult(moveInCost)
+    }
+    else{
+      return setResult(0)
+    }
+  }
 
-        <select>
-          <option value=''></option>
-          <option>Move-in</option>
-          <option>Move-out</option>
-        </select>
-  
-      
-        <button>Calculate</button>
 
-        <h2>Amount owed</h2>
+    // console.log({ startDate })
+    // console.log({ currentDate })
+    // // console.log({currentMonth})
+    // // console.log({currentYear})
+    // console.log({ daysInCurrentMonth })
+    // console.log({ ratio: moveInRatio })
 
-        <div></div>
-      </header>
-      <body>
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="https://cdn-icons-png.flaticon.com/512/5351/5351550.png" className="App-logo" alt="logo" />
+          <h1>Rent Proration Calculator</h1>
 
-      </body>
-    </div>
-  );
-}
+          <div>
+            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+          </div>
 
-export default App;
+          <input type="text" placeholder="Cost" onChange={handleCost}></input>
+
+          <select onChange={handleMoveStatus}>
+            <option value=''>Select</option>
+            <option value="moveIn">Move-in</option>
+            <option value="moveOut">Move-out</option>
+          </select>
+
+
+          <button onClick={handleCalculate}>Calculate</button>
+
+          <h2>Amount owed</h2>
+          {result}
+          <div></div>
+        </header>
+        <body>
+
+        </body>
+      </div>
+    );
+  }
+
+  export default App;
