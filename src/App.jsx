948633dd130f844771c84c2cd83541b52302d4cd
BobbyWhitacre27 <bobby.whitacre27@gmail.com>
2023-04-06 23:18:43 -0400

    Removed extra button

--- src/App.jsx
+++ src/App.jsx
@@ -83,10 +83,6 @@ function App() {
           <h2>Amount owed</h2>
           <h2>{resultCurrency}</h2>
           </div>
-     
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-  Button
-</button>
         </header>
       </div>
     );
